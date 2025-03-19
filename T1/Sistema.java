9553f4989b5d7e1e22284bba6b9ebd4acd22b80b
Giovani Cancherini <giovani.canquerini@gmail.com>
2025-03-19 20:04:08 -0300

    feat: implement 1.2 methods

--- T1/Sistema.java
+++ T1/Sistema.java
@@ -37,9 +37,36 @@ public class Sistema {
 		public int frames = tamMem/tamPag;
 
 		public Boolean aloca(int nroPalavras, int[] tabelaPaginas){
-			return false;
+			// retorna true se consegue alocar ou falso caso negativo
+ 			// cada posição i do vetor de saída “tabelaPaginas” informa em que frame a página i deve ser hospedada 
+			
+			int paginasNecessarias = (int) Math.ceil((double) nroPalavras / tamPag);
+			int paginasAlocadas = 0;
+			
+			for (int frame = 0; frame < frames; frame++) {
+				if (paginasAlocadas < paginasNecessarias && tabelaPaginas[frame] == -1) {
+					tabelaPaginas[paginasAlocadas] = frame;
+					paginasAlocadas++;
+				}
+			}
+			
+			if (paginasAlocadas == paginasNecessarias) {
+				// todas as paginas foram alocadas
+				return true;
+			} else {
+				// libera os espaços que já foram alocados
+				for (int i = 0; i < paginasAlocadas; i++) {
+					tabelaPaginas[i] = -1;
+				}
+				return false;
+			}
 		}
-		public void desaloca(int[] tablaPaginas){
+		public void desaloca(int[] tabelaPaginas){
+			// libera os frames alocados 
+
+			for (int i = 0; i < tabelaPaginas.length; i++) {
+				tabelaPaginas[i] = -1;
+			}
 			return;
 		}
 	}
